5c7a8646a7e07b6add79a17e4b35864638e46f3b
floushi <floushi69@gmail.com>
2025-04-04 01:53:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -147,7 +147,6 @@ async function addSignatureToPage(page, signatureConfig, signatureData, fields, 
 }
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
